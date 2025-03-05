f9d424a52c95db95d3e7a558f391d75591829b47
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-03-05 06:34:46 +0500

    Update JobController.java

--- src/main/java/com/ansartech/springbootrest/JobController.java
+++ src/main/java/com/ansartech/springbootrest/JobController.java
@@ -1,33 +1,37 @@
 package com.ansartech.springbootrest;
-
 import com.ansartech.springbootrest.model.JobPost;
 import com.ansartech.springbootrest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController 
 @CrossOrigin(origins = "http://localhost:3000")
 public class JobController {
-
-
     @Autowired
     JobService serv ;
-
     @GetMapping("jobPost")
     public List<JobPost>viewJob(){
         return serv.viewJobs();
     }
-
     @GetMapping("jobPost/{postId}")
     public JobPost getJob(@PathVariable int postId){
         return serv.getJob(postId);
     }
-
     @PostMapping("jobPost")
-    public void addJob(@RequestBody JobPost jb){
+    public JobPost addJob(@RequestBody JobPost jb){
         serv.addjob(jb);
+        return serv.getJob(jb.getPostId()-1);
+    }
+    @PutMapping("jobPost")
+    public JobPost modifyJob(@RequestBody JobPost jp){
+
+        return serv.updateJob(jp);
+    }
+
+    @DeleteMapping("jobPost")
+    public void removeJob(@RequestBody JobPost jp){
+
+        serv.deleteJob(jp);
     }
 
 
